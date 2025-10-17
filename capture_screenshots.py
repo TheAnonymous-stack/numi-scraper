dbd30dda8663343a9720a5443082c99e239476b1
TheAnonymous-stack <thanhnht.2014@gmail.com>
2025-10-17 08:42:53 -0400

    modified capture_screenshots.py

--- capture_screenshots.py
+++ capture_screenshots.py
@@ -18,7 +18,7 @@ driver = webdriver.Chrome(options=chrome_options)
 
 # Get all HTML files in the HTML directory
 grade = 8
-html_files = glob.glob("HTML/Gr8_39_E4_4_*.html")
+html_files = glob.glob("HTML/Gr8_24_E3_3_*.html")
 html_files.sort()
 
 print(f"Found {len(html_files)} HTML files to process")
